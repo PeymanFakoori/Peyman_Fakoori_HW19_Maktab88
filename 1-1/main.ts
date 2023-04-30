117374b60f036ed36c5ca0f573e077085a76b74f
Payman <115703785+PeymanFakoori@users.noreply.github.com>
2023-04-30 19:00:47 +0330

    car race added

--- 1-1/main.ts
+++ 1-1/main.ts
@@ -2,13 +2,15 @@
 class Car {
   name: string;
   position: number;
+  placment: number;
   constructor(name: string, position: number) {
     this.name = name;
     this.position = position;
+    this.placment = this.placment;
   }
 }
-const cars: Car[] = [];
-let numberOfCars: number;
+// let numberOfCars: number;
+let numberOfCars: string | null = prompt("please enter number of cars");
 
 function randomOrder(inputCarCount: number): number[] {
   let randomNumbers: number[] = [];
@@ -29,3 +31,55 @@ function stepArray(Max: number, inputCount: number): number[] {
   }
   return stepOrder;
 }
+
+function carRace(numberOfCars: number) {
+  let road: string[] = [..."*".repeat(300)];
+  let cars: Car[] = [];
+  for (let index = 0; index < numberOfCars; index++) {
+    let name: string | null = prompt(
+      `Please enter name of ${index + 1} car's name `
+    );
+    if (!name) {
+      throw new Error("name can not be empty");
+    }
+    cars.push(new Car(name));
+  }
+  let stepOrder = randomOrder(numberOfCars);
+  cars.forEach((item, index) => {
+    item.position = randomOrder[index];
+    item.placment = 0;
+  });
+  cars.sort((a, b) => {
+    return (a.position = b.position);
+  });
+  console.log(stepOrder);
+  cars.forEach((element) => {
+    console.log(element.name);
+  });
+  let result: string[] = [];
+  while (
+    cars.filter((item) => {
+      return item.placment > 300;
+    }).length < numberOfCars
+  ) {
+    let steps = stepArray(10, numberOfCars);
+    for (let index = 0; index < numberOfCars; index++) {
+      cars[index].placment += steps[index];
+      if (cars[index].placment < 300) {
+        road[cars[index].placment] = cars[index].name;
+        cars.filter((element) => {
+          return (
+            element.placment === cars[index].placment &&
+            element.name !== cars[index].name
+          );
+        });
+      } else {
+        result.push(cars[index].name);
+      }
+    }
+    console.log(road.join(""));
+  }
+  console.log(`the winers is : ${result[0]}`);
+}
+
+carRace(Number(numberOfCars));
