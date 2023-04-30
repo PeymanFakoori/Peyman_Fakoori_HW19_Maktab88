e5273b5280656f375b290a55afa600f1feb1eecf
Payman <115703785+PeymanFakoori@users.noreply.github.com>
2023-04-30 15:36:26 +0330

    car 1

---
+++ 1-1/main.ts
@@ -0,0 +1,10 @@
+//========== Car Class ==========
+class Car {
+  name: string;
+  position: number;
+  constructor(name: string, position: number) {
+    this.name = name;
+    this.position = position;
+  }
+}
+const cars: Car[] = [];
